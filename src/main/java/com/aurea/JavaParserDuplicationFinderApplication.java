0a33e1689fa6c4d11c2e5196f05582d4e9913f43
Andrey Artyukhov <otanikotani@gmail.com>
2017-03-02 11:55:25 +0700

    First commit

---
+++ src/main/java/com/aurea/JavaParserDuplicationFinderApplication.java
@@ -0,0 +1,12 @@
+package com.aurea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaParserDuplicationFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaParserDuplicationFinderApplication.class, args);
+	}
+}
